f6642321c229967d705c0bfb6634e86338737df2
Jason Walsh <2184329+jasonwalsh@users.noreply.github.com>
2018-12-05 20:36:29 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {}
+
+resource "random_pet" "pet" {}
+
+resource "aws_lambda_function" "lambda_function" {
+  description   = "${var.description}"
+  function_name = "${random_pet.pet.id}"
+  handler       = "${var.handler}"
+  memory_size   = "${var.memory_size}"
+  runtime       = "${var.runtime}"
+  timeout       = "${var.timeout}"
+}
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "description" {
+  default     = ""
+  description = "A description of the function"
+}
+
+variable "handler" {
+  description = "The name of the method within your code that Lambda calls to execute your function"
+}
+
+variable "memory_size" {
+  default     = 128
+  description = "The memory allocated to the function"
+}
+
+variable "runtime" {
+  description = "The runtime version for the function"
+}
+
+variable "timeout" {
+  description = "The amount of time that Lambda allows a function to run before terminating it"
+}
